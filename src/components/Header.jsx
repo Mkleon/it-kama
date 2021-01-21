a868ea279dfb84c08979fdfa2bf4a832bc0ac584
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2021-01-21 22:44:51 +0300

    begit project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img alt='logo' src='https://www.logodesign.net/images/logo.png' />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img alt='peru' src='https://im0-tub-ru.yandex.net/i?id=3ee12bfb49fb9fd3670cf996dac7e0ab&n=13'/>
+      </div>
+      <div>
+        ava
+      </div>
+      <div>
+        posts
+        <div>
+        new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 1
+          </div>
+        </div>
+      </div>          
+    </div>
+  );
+};
+
+export default Profile;
