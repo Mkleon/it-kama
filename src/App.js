66cf8ca8df1357ea9cf7d7057b3caebcefeb06f9
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2021-01-21 21:07:23 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <a href='/'>Home</a>
+        <a href='/'>News feed</a>
+        <a href='/'>Messages</a>
+      </ul>
+    </div>);
+};
+
+export default Header;
