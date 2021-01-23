13d6508117f183b15aaaf1ce7065320daa179079
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2021-01-23 22:33:17 +0300

    lesson 18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <div>
         <img className={classes.banner} alt='peru' src='https://im0-tub-ru.yandex.net/i?id=3ee12bfb49fb9fd3670cf996dac7e0ab&n=13'/>
       </div>
