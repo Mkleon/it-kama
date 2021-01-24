4b14ae554fab1c8b993b907794d2bc7ec4848093
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2021-01-24 12:23:09 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,33 @@ import classes from './Dialogs.module.css';
 const Dialogs = (props) => {
   return (
     <div>
-      Dialogs
+      <div className={classes.dialogs}>
+        <div className={classes.dialogsItems}>
+          <div className={classes.dialog + ' ' + classes.active}>
+            Dima
+          </div>
+          <div className={classes.dialog}>
+            Sasha
+          </div>
+          <div className={classes.dialog}>
+            Sveta
+          </div>
+          <div className={classes.dialog}>
+            Andrey
+          </div>
+        </div>
+        <div className={classes.messages}>
+          <div className={classes.message}>
+            Hi!
+          </div>
+          <div className={classes.message}>
+            Hi, Piter!
+          </div>
+          <div className={classes.message}>
+            How are you?
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
