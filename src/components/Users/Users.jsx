ff54021849ee84a03a24dadf33a7c62390ca1246
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2021-02-08 21:45:27 +0300

    lesson 51-1

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import User from './User/User';
+import * as axios from 'axios';
 
 const Users = (props) => {
     if (props.users.allIds.length === 0) {
-        props.setUsers([
-            { id: 1, name: 'Piter', followed: false, location: { country: 'Russia', city: 'Tula' }, avatar: 'https://www.popsci.com/resizer/oBw2zifFCqH3deU6vy2bPRSG99Q=/760x456/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/WMD5M52LJFBEBIHNEEABHVB6LA.jpg' },
-            { id: 5, name: 'Sergey', followed: false, location: { country: 'Belarus', city: 'Minsk' }, avatar: 'https://i.pinimg.com/originals/44/5e/fc/445efcdd7459264333a37ff0cf1febd1.jpg' },
-            { id: 12, name: 'Sveta', followed: true, location: { country: 'Russia', city: 'Moscow' }, avatar: 'https://i.pinimg.com/736x/35/65/d6/3565d6559ee16e40137483c8391b5bf9.jpg' },
-            { id: 14, name: 'Andrey', followed: true, location: { country: 'USA', city: 'Orlando' }, avatar: 'https://i.redd.it/6xgyra4ztzs41.png' },
-        ]);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then((response) => {
+                props.setUsers(response.data.items);
+            });
     }
 
     return (
