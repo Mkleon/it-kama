dd37a8a68ff5d74daa12b34d86d98ade8db9f3a0
Kirill Miklashevsky <klmiklashevsky@gmail.com>
2021-02-10 18:28:57 +0300

    lesson 52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,16 +3,19 @@ import User from './User/User';
 import * as axios from 'axios';
 
 const Users = (props) => {
-    if (props.users.allIds.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then((response) => {
-                props.setUsers(response.data.items);
-            });
-    }
+    const getUsers = () => {
+        if (props.users.allIds.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then((response) => {
+                    props.setUsers(response.data.items);
+                });
+        }
+    };
 
     return (
         <div>
-           {props.users.allIds.map((id) => <User user={props.users.byId[id]} follow={props.follow} unfollow={props.unfollow} key={id} />)}
+            <button onClick={getUsers}>Get users</button>
+            {props.users.allIds.map((id) => <User user={props.users.byId[id]} follow={props.follow} unfollow={props.unfollow} key={id} />)}
         </div>
     )
 };
