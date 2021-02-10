4f8226c25c96f509ae0cad8a2b4e36cdd77e80b7
Kirill Miklashevsky <klmiklashevsky@gmail.com>
2021-02-10 19:05:57 +0300

    lesson 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import User from './User/User';
 import * as axios from 'axios';
 
-const Users = (props) => {
-    const getUsers = () => {
-        if (props.users.allIds.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then((response) => {
-                    props.setUsers(response.data.items);
-                });
-        }
-    };
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        alert('dsfs');
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+            });
 
-    return (
-        <div>
-            <button onClick={getUsers}>Get users</button>
-            {props.users.allIds.map((id) => <User user={props.users.byId[id]} follow={props.follow} unfollow={props.unfollow} key={id} />)}
-        </div>
-    )
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.users.allIds.map((id) => <User 
+                    user={this.props.users.byId[id]}
+                    follow={this.props.follow}
+                    unfollow={this.props.unfollow} key={id}
+                />)}
+            </div>
+        )
+    }
 };
 
 export default Users;
