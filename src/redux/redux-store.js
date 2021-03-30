fbcb18b2264a5caf8d8c6019b826c02775f82df1
Kirill <kirillo81@mail.ru>
2021-03-30 20:03:02 +0300

    lesson 91

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore, compose } from 'redux';
 import profileReducer from './profileReducer';
 import usersReducer from './usersReducer';
 import friendsReducer from './friendsReducer';
@@ -19,6 +19,7 @@ const reducers = combineReducers({
   app: appReducer
 });
 
-const store = createStore(reducers, applyMiddleware(ThunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(ThunkMiddleware)));
 
 export default store;
